d6921a6ad41ef997dcf9e50e9079869ce39c03a3
root <viviane.desgrange@insa-rouen.fr>
2014-06-02 14:37:40 +0200

    fuck

--- src/main/java/fr/insarouen/poneysdoux/server/Information.java
+++ src/main/java/fr/insarouen/poneysdoux/server/Information.java
@@ -38,7 +38,7 @@ public class Information {
 
 		  Iterator<String[]> it = this.users.iterator();
 		  while(it.hasNext()){
-			  String[] user_final = new String[6];
+			  String[] user_final = new String[7];
 			  String[] user = it.next();
 			  
 			  user_final[0] = user[0];
@@ -47,6 +47,7 @@ public class Information {
 			  user_final[3] = user[3];
 			  user_final[4] = user[4];
 			  user_final[5] = user[5];
+			  user_final[6] = user[6];
 			  users_final.add(user_final);
 		  }
 		  return(users_final);
